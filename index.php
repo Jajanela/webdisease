6677fea63d9a652f9b29f0aef7fef276a84d3907
Janela Tamayo <22ur0776@psu.edu.ph>
2023-12-10 07:14:15 +0800

    init

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/979ee355d9.js" crossorigin="anonymous"></script>
+    <title>Home</title>
+</head>
+<body>
+
+    <header>
+        <div class="top-section">
+            <img class="logo" src="images/logo.png" alt="PSU Logo">
+            <label><b>BOARDING</b><span class="brand-name"> BEACON</span></label> 
+            
+        </div>
+        <div class="system-name">
+            <label for=""><b>BOARDING HOUSE FINDER</b></label>
+            <button type="submit" id="logout">LOGOUT</button>
+        </div>
+    
+        
+    </header>
+    
+    <nav class="navi">
+        <ul>
+            <li id="left-nav">
+                <a href="" id="selected">Search</a>
+               
+            </li>
+        </ul>
+        <ul>
+            <li id="project">
+                <a href=""><i class="fa-solid fa-house"></i>&nbsp&nbspMy Boarding</a>
+            </li>
+        </ul>
+    </nav>
+    
+    <div class="content">
+            <h1>Michelle Apartelle</h1>
+            <p class="par">PSU Village, San Vicente Urdaneta City, Pangasinan</p>
+            <p class="par">The boarding house has two spacious rooms and plenty
+                of space. It includes a storage closet and can accomodate
+                up to seven people.
+            </p>
+            <!-- <h4> Available Units : 3 Units </h4>
+            <h4> Capacity Every Unit : 7 People</h4>
+            <h4>Owner's Name : Michelle Dee</h4>
+            <h4> Owner's Number : 09123456789 </h4> -->
+            <a href="#" class="btn">Rent Now</a>
+        </div>
+        <img src="images/boarding.png" class="image">
+    </div>
+    <div class="cont">
+            <h4 class="unit"> Available Units : 3 Units </h4>
+            <h4 class="owner">Owner's Name : Michelle Dee</h4>
+            <!-- <h4 class="name">Owner's Name : Michelle Dee</h4>
+            <h4 class="num"> Owner's Number : 09123456789 </h4> -->
+        </div>
+<div class="contt">
+        <h4 class="owner"> Capacity Every Unit : 7 People</h4>
+        <h4 class="unit"> Owner's Number : 09123456789 </h4>
+       
+        </div>
+    
+</body>
+</html>
